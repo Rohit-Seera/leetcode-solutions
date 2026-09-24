875568b66b3be1a09d4eadb0fb178058f32c32bc
Rohit Meena <rrrohitmeena@gmail.com>
2026-09-25 03:36:36 +0530

    Add LeetCode 136 Single Number solution

---
+++ Arrays/136-Single-Number.cpp
@@ -0,0 +1,18 @@
+// LeetCode 136 - Single Number
+// Difficulty: Easy
+// Approach: XOR
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans = ans ^ nums[i];
+        }
+
+        return ans;
+    }
+};
